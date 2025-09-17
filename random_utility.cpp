f564436ea87e5bbd79326bd5de4edba3fbf851b9
Dpbm <dpbm136@gmail.com>
2025-09-17 16:12:58 -0300

    plots for cpp version

--- random_utility.cpp
+++ random_utility.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 #include <random>
+#include <vector>
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h> 
 
 namespace py = pybind11;
 
 class RandomUtility{
     public:
+        RandomUtility(){
+            this->random_seed();
+        }
+
         void random_seed(){
-            std::cout << "generating seed....\n";
+            //std::cout << "generating seed....\n";
             this->set_seed(this->rd());
         }
 
         void set_seed(size_t new_seed){
-            std::cout << "Setting seed to: " << new_seed << "\n";
+            //std::cout << "Setting seed to: " << new_seed << "\n";
             this->seed = new_seed;
             this->rng.seed(new_seed);
         }
 
-        size_t get_random_value(){
+        size_t get_random_value_by_array(const std::vector<double> &values){
+            return std::discrete_distribution<size_t>(values.begin(), values.end())(this->rng);
+        }
+
+
+        size_t get_random_value_default(){
             // both 0 and 1 has 50% of chance
             return std::discrete_distribution<size_t>({0.5, 0.5})(this->rng);
         }
@@ -40,6 +51,7 @@ PYBIND11_MODULE(random_utility, m, py::mod_gil_not_used()) {
         .def(py::init<>())
         .def("random_seed", &RandomUtility::random_seed)
         .def("set_seed", &RandomUtility::set_seed)
-        .def("get_random_value", &RandomUtility::get_random_value)
+        .def("get_random_value", &RandomUtility::get_random_value_default)
+        .def("get_random_value_by_array", &RandomUtility::get_random_value_by_array)
         .def_property_readonly("seed", &RandomUtility::get_seed);
 }
