0f9d5cb2480ae0e869d9c7bbd6d5ce8e7c96b2f3
Dpbm <dpbm316@gmail.com>
2026-01-22 10:50:41 -0300

    updated dot scripts

--- dot-scripts.sh
+++ dot-scripts.sh
@@ -6,8 +6,11 @@ set -o pipefail
 SCRIPT_DIR="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 source $SCRIPT_DIR/utils.sh
 
+TARGET_ASSETS_FOLDER="$SCRIPT_DIR/assets"
+
 for dot_file in $SCRIPT_DIR/*.dot; do
-	OUTPUT_FILE_NAME=$(sed 's|\.dot|.png|g' <<< $dot_file)
-	print_blue_line "running $dot_file and saving output in $OUTPUT_FILE_NAME..."
-	dot -Tpng $dot_file >> "$OUTPUT_FILE_NAME"
+	OUTPUT_FILE_NAME=$(sed 's|\.dot|.png|g' <<< $dot_file | sed 's|^.*\/||g' )
+	OUTPUT_FILE_PATH="$TARGET_ASSETS_FOLDER/$OUTPUT_FILE_NAME"
+	print_blue_line "running $dot_file and saving output in $OUTPUT_FILE_PATH..."
+	dot -Tpng $dot_file >> "$OUTPUT_FILE_PATH"
 done
